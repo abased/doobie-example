4b3658845d84b87b98c8ce9a13cca31e17896f16
nickdeyoung <ndeyoung@sosacorp.com>
2017-02-21 14:53:06 -0800

    make a simple example of self referencing classes

--- greylock-service/src/main/scala/com/abased/example/IntellijExample.scala
+++ greylock-service/src/main/scala/com/abased/example/IntellijExample.scala
@@ -1,45 +1,20 @@
 package com.abased.example
 
-/**
-  * Created by nickdeyoung on 11/3/16.
-  */
+import classy.generic._
+import classy.config._
+import com.typesafe.config.{ Config, ConfigFactory }
 
 case class Node(id:String, label:String, edges:List[Edge])
 case class Edge(from:Node, to:Node, label:String)
 case class Graph(nodes:List[Node], edges:List[Edge])
 case class MyGraphConfig(graph: Graph)
 
-
+/**
+  * Created by nickdeyoung on 11/3/16.
+*/
 object IntellijExample extends App {
 
-
-  import classy.generic._
-
-  import classy.config._
-  import com.typesafe.config.{ Config, ConfigFactory }
-
-
-  // can I make a decoder for my graph?
+  // can I make a decoder for my graph, which has self referencing classes?
   val decoder1: ConfigDecoder[MyGraphConfig] = deriveDecoder[Config, MyGraphConfig]
-
-//
-//  val stringXwhichLookslikeJSONButIsnt = "mine = [ { myval:0, neighbor: { myval: 9, neighbor: null }} ]"
-//  val rawConfig = ConfigFactory.parseString(stringXwhichLookslikeJSONButIsnt)
-//  val decoded = decoder1(rawConfig)
-//
-//  decoded match {
-//    case Left(err) =>
-//      println(err)
-//    case Right(MyConfig(mines)) =>
-//      println(mines)
-//  }
-
-
-  ()
-
-
-
-
-
 
 }
